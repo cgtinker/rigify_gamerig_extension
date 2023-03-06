68b04932ce547f323cfc225de7ad890fa603d2a4
dhsu <=>
2023-03-06 09:49:28 +0100

    zip setup

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+name="rigify_gamerig_extension"
+version="_release_001"
+dirpath=$(pwd)
+dirpath=${dirpath:-/}
+dirname="${dirpath##*/}"
+echo $dirname
+
+zipcmd() {
+    zip -r $dirname/$name$version.zip $dirname \
+    -x "__MAXOSX/*" -x "*.DS_Store" \
+    -x "*venv*" -x "*.idea*" -x "*.git*" \
+    -x "*__pycache__*" -x "*docs*" -x "*setup*" \
+    -x "*swp", -x "*test*"
+}
+
+cd ..
+echo $(zipcmd)
+# zipcmd
+# cd $name
